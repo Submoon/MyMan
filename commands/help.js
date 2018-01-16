b091241124db4b3f1502a1688fabb122a3916abc
BAL-FONTAINE <fabien.bal-fontaine@econocom.com>
2018-01-16 16:01:43 +0100

    better help : help for commands

--- commands/help.js
+++ commands/help.js
@@ -18,14 +18,44 @@ module.exports = class HelpCommand{
     }
 
     async run() {
+        const nb = Number(this.args[0]);
+        let embed = null;
+        if(this.args.length==0 || !isNaN(nb)){
+            let page = nb ? nb : 1;
+            embed = this.getHelpPageEmbed(page);
+        }else{
+            embed = this.getHelpForCommand(this.args[0]);
+        }
+        
 
-        let page = this.args[0] ? this.args[0] : 1;
-
-        let embed = this.getHelpPageEmbed(page);
+        
       
        this.message.channel.send({embed});
 
         
+    }
+
+    getHelpForCommand(commandName){
+        let lowerCommandName = commandName.toLowerCase();
+        let command = this.client.commands[lowerCommandName];
+        let embed = new Discord.RichEmbed()
+        .setAuthor(this.client.user.username, this.client.user.avatarURL)
+        /*
+         * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+         */
+        .setColor(0x00AE86)
+        .setDescription("A list of all commands available for the bot");
+        if(command){
+            embed = embed.setTitle(lowerCommandName)
+            .setDescription(command.description.text)
+            .addField("Usage", command.description.usage);
+        }else{
+            embed = embed.setTitle(`Unknown command ${lowerCommandName}`)
+            .setDescription(`Unknown command ${lowerCommandName}`);
+        }
+        return embed;
+
+
     }
 
     getHelpPageEmbed(page){
@@ -47,14 +77,16 @@ module.exports = class HelpCommand{
          */
         .setColor(0x00AE86)
         .setDescription("A list of all commands available for the bot")
-        .setFooter(`Page ${currentPage+1} out of ${numberOfPages}`)
+        .setFooter(`Page ${currentPage+1} out of ${numberOfPages}`);
 
         let i = 0;
+        //25 max fields...
         let mini = currentPage*25;
         let maxi = mini+24;
         let commandsToPrint = entries(this.client.commands)/*.slice(mini, maxi)*/;
 
         for (let [commandName, command] of commandsToPrint) {
+            //Can't slice an associative array, this is a nightmare
             if(i < mini){
                 i++;
                 continue;
@@ -63,11 +95,15 @@ module.exports = class HelpCommand{
                 break;
             }
 
-            let description = command.description ? `${command.description.text}; \nusage: ${command.description.usage}` : "No description provided";
+            let description = this.getStringDescriptionOfCommand(command);
             embed = embed.addField(commandName,  description, true);
             
         }
         return embed;
+    }
+
+    getStringDescriptionOfCommand(command){
+        return command.description ? `${command.description.text}; \nusage: ${command.description.usage}` : "No description provided";
     }
 
 
