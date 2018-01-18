389eb21c26aaab33bcb9038054a9dfea60079d4b
BAL-FONTAINE <fabien.bal-fontaine@econocom.com>
2018-01-18 13:57:44 +0100

    Began the work on dropbox API

--- commands/emojis.js
+++ commands/emojis.js
@@ -1,6 +1,7 @@
 "use strict";
 const axios = require("axios");
 const fs = require("fs");
+const Dropbox = require("dropbox");
 module.exports = class EmojiCommand{
 
     constructor(client, message, args){
@@ -17,29 +18,37 @@ module.exports = class EmojiCommand{
     }
 
     async run() {
-        let path =  './emojis';
-        if (!fs.existsSync(path)){
-            fs.mkdirSync(path);
-        }
-       this.message.guild.emojis.forEach(emoji =>axios.request({
-        responseType: 'arraybuffer',
-        url: emoji.url,
-        method: 'get',
-        headers: {
-          'Content-Type': 'image',
-        },
-      }).then((result) => {
-        let name = emoji.name;
-        let path =  './emojis/';
-        const outputFilename = path+'/'+name+'.png';
-        fs.writeFile(outputFilename, result.data, (err) =>{
-            if(err) console.error(err);
-            
+        var dbx = new Dropbox({ accessToken: this.client.config.dropboxAccessToken });
+
+        // let path =  './emojis';
+        // if (!fs.existsSync(path)){
+        //     fs.mkdirSync(path);
+        // }
+        // A remplacer par la vérif de dossier de dropbox/création de dossier
+        let guild = this.message.guild;
+        guild.emojis.forEach(emoji => {
+            let urlArray = emoji.url.split(".");
+            let ext = urlArray[urlArray.length-1];
+           axios.request({
+            responseType: 'arraybuffer',
+            url: emoji.url,
+            method: 'get',
+            headers: {
+            'Content-Type': 'image',
+            }
+            }).then((result) => {
+                let name = emoji.name;
+                let path =  `./emojis/${guild.name}/`;
+                const outputFilename = `${path}/${name}.${ext}`;
+                // fs.writeFile(outputFilename, result.data, (err) =>{
+                //     if(err) console.error(err);
+                // });
+                // A remplacer par l'écriture de fichier sur dropbox
     
-      });
+            });
       
-        }
-      ))}
+        });
+    }
 
 
 
