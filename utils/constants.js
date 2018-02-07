3619fae11f8e03e6d188375c9a03263b81da2a8a
ROS Quentin <quentin.ros@sword-group.com>
2018-02-07 17:12:43 +0100

    - Work in progress. Constants is created and functional for color_list.

---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+"use strict";
+
+class constantsObject{
+
+    constructor(){
+        //this.array = array;
+        this.colorok = "DEFAULT, AQUA, GREEN, BLUE, PURPLE, GOLD, ORANGE, RED, GREY, NAVY, DARK_AQUA, DARK_GREEN, DARK_BLUE, DARK_PURPLE, DARK_GOLD, DARK_ORANGE, DARK_RED, DARK_GREY, DARKER_GREY, LIGHT_GREY, DARK_NAVY, BLURPLE, GREYPLE, DARK_BUT_NOT_BLACK, NOT_QUITE_BLACK, RANDOM";
+    }
+}
+
+let constants = new constantsObject();
+
+module.exports = constants;
