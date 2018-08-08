f5431f65659f21f8c848ca8c0ce443babef1e60d
FABIEN BAL-FONTAINE <fabien.bal-fontaine@econocom.com>
2018-08-08 13:56:13 +0200

    Using message timestamps for more accuracy for the ping command

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,12 @@ const logger = require("../utils/logger");
 
 module.exports = class PingCommand{
 
+    /**
+     * 
+     * @param {Client} client 
+     * @param {Message} message 
+     * @param {string[]} args 
+     */
     constructor(client, message, args){
         this.client = client;
         this.message = message;
@@ -17,10 +23,10 @@ module.exports = class PingCommand{
     }
 
     async run() {
-        let startTime = +new Date();
+        let createdAt = this.message.createdTimestamp;
         let message = await this.message.channel.send("Pong!");
-        let endTime = +new Date();
-        let diff = endTime-startTime;
+        let endTime = message.createdTimestamp;
+        let diff = endTime-createdAt;
         message.edit(`Pong (${diff}ms)`)
         logger.info(`Received ping command, ping = ${diff}ms`)
     }
